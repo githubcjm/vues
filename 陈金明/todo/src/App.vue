38f9aea2342b28ead315c43eaf14f240d4128b3b
githubcjm <418732318@qq.com>
2019-07-30 00:29:41 +0800

    day6

---
+++ 陈金明/todo/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>Todo App</h1>
+    <AddTodo />
+    <Todos v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+import AddTodo from "./components/Todo/AddTodo.vue";
+import Todos from "./components/Todo/Todos.vue";
+export default {
+  //M层
+  name: "app",
+  components: {
+    AddTodo,
+    Todos
+    //注册
+  },
+  data() {
+    //数据
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "todo one",
+          complated: false
+        },
+        {
+          id: 2,
+          title: "todo two",
+          complated: true
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ 陈金明/todo/src/components/Todo/Todos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="todos">
+    <h2>todos</h2>
+    <div v-for="item in todos">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    //数据
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "todo one",
+          complated: false
+        },
+        {
+          id: 2,
+          title: "todo two",
+          complated: true
+        }
+      ]
+    };
+  }
+};
+</script>
+
